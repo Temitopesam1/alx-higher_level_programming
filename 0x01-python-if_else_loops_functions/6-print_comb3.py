585e2fcd62020fff53d4507ffe8b98eaaccc402c
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-08-23 17:14:42 -0700

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(0,10):
+for i in range(0, 10):
+    for j in range(0, 10):
         if i < j:
             if i == 8 and j == 9:
-                print(F"{i}{j}")
+                print("{}{}".format(i, j))
             else:
-                print(f"{i}{j}, ", end="")
+                print(f"{i}{j}".format(i, j), end=", ")
