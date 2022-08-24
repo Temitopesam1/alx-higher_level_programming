66c0dfd2e5a2f80914bc4e3acb66e47d3b68b618
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-08-23 17:20:07 -0700

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-        for i in range(len(str)):
-                asci = ord(str[i])
-                if asci in range(97, 123):
-                        asci -= 32
-                print(f"{chr(asci)}", end="")
-        print()
+    for i in range(len(str)):
+        asci = ord(str[i])
+        if asci in range(97, 123):
+            asci -= 32
+       print(f"{}".format(chr(asci)), end="")
+       print()
