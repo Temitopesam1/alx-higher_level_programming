72c80a624e7e6f418f18ab9bb850316130e31a4a
Temitopesam1 <temitopesam5@gmail.com>
2022-08-28 13:13:16 -0700

    Python

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-<<<<<<< HEAD
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
@@ -8,9 +7,3 @@ def replace_in_list(my_list, idx, element):
     else:
         my_list[idx] = element
         return my_list
-=======
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
->>>>>>> 44c7d9cf9f2a463e697ff8d7e1e67459377c43e1
