1d76b7eeea069b05b1cdd73ea56a6be95c096b50
Temitopesam1 <temitopesam5@gmail.com>
2022-12-15 17:05:42 -0800

    fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """ Finds a peak inside a list """
 
 
-def find_peak(list_of_integers):
+"""def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
@@ -23,3 +23,27 @@ def find_peak(list_of_integers):
     if li[mid + 1] > li[mid - 1]:
         return find_peak(li[mid:])
     return find_peak(li[:mid])
+"""
+
+
+def find_peak(list_of_integers):
+    if list_of_integers == []:
+        return None
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    mid = length // 2
+    li = list_of_integers
+    if mid == length - 1:
+        if li[0] > li[1]:
+            return li[0]
+        else:
+            return li[1]
+    elif li[mid] > li[mid - 1] and li[mid] > li[mid + 1]:
+        return li[mid]
+    elif li[mid - 1] >= li[mid]:
+        return find_peak(li[:mid])
+    elif li[mid + 1] >= li[mid]:
+        return find_peak(li[mid:])
+    elif li[length - 1] > li[lenght - 2]:
+        return li[lenght - 1]
