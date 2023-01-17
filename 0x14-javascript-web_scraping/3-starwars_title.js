3636e5786e157c38a2118b3b4d21e23aa483a0aa
Temitopesam1 <temitopesam5@gmail.com>
2023-01-17 08:38:12 -0800

    second push

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const id = process.argv[2];
 request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
   if (error == null) {
-    const json = JSON.parse(body);
+    const json = JSON.parse(body.body);
     console.log(json.title);
   }
 });
