33489b5165b4715f550eb26ac8af7fef1c6685a6
Temitopesam1 <temitopesam5@gmail.com>
2023-01-17 08:43:46 -0800

    third push

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body.body);
-    console.log(json.title);
-  }
+request('http://swapi.co/api/films/' + id, (error, body) => {
+	if (error) {
+	  console.error(error);
+	  return;
+	}
+	const json = JSON.parse(body.body);
+	console.log(json.title);
 });
